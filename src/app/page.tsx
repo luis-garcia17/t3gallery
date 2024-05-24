be18553939d55bdc0453ade6bd34fb2180f12bd5
Luis Garcia <luis.lag17@gmail.com>
2024-05-24 15:58:04 -0600

    database connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import { db } from "../server/db";
 const mockUrls = [
   "https://utfs.io/f/cf004fcf-e6ae-44f0-b866-1bb31c9968c8-wpgxpo.png ",
   "https://utfs.io/f/3b5cddb1-bb39-416b-9e57-30014fa409b5-539iyw.jpeg",
@@ -12,10 +12,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany()
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            <p>{post.name}</p>
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
